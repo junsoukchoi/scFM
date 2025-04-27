4e5a77905619df4302f12688869a4343697da796
Seung Hoon Han <syshhan5172@naver.com>
2025-04-27 23:03:00 +0900

    Edit NAMESPACE using Roxygen.

---
+++ R/Roxygen_setup.R
@@ -0,0 +1,8 @@
+#' @useDynLib scFM, .registration=TRUE
+#' @import Rcpp
+#' @importFrom stats rgamma rnorm runif
+#' @importFrom statmod rinvgauss
+#' @import GIGrvg
+#' @import tmvnsim
+#' @import TruncatedNormal
+NULL
