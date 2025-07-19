063449a74b9ab4d72a54dbc4d13d898e088b2d03
Junsouk Choi <junsoukchoi@Junsouks-Mac-Studio.local>
2025-07-19 22:07:31 +0900

    Update the names of some model parameters in the gibbs_scFM function.

--- R/scFM.R
+++ R/scFM.R
@@ -3,8 +3,8 @@
 #' @param x An n-by-p matrix where each row represents the read counts for a single cell in single-cell sequencing data.
 #' @param k An integer indicating the maximum allowable number of latent factors.
 #' @param m An integer specifying a model hyperparameter in scFM that serves as a threshold to distinguish between inflated low counts and typical higher counts in single-cell sequencing data.
-#' @param starting A list of initial values for the data-augmented Gibbs sampler for scFM. The list should include the tags \code{Z}, \code{delta}, \code{Lambda}, \code{U}, \code{Sigma}, \code{Phi}, \code{tau}, and \code{Psi}, each representing the initial MCMC value for the corresponding scFM parameter.
-#' @param priors A list of hyperparameter values. Valid tags are \code{Sigma} and \code{Phi}, specifying the hyperparameters for the inverse-gamma prior on error variances (i.e., \eqn{a_{\sigma}}, \eqn{b_{\sigma}}) and the Dirichlet-Laplace prior on factor loadings (i.e., \eqn{\alpha}), respectively.
+#' @param starting A list of initial values for the data-augmented Gibbs sampler for scFM. The list should include the tags \code{Z}, \code{delta}, \code{Lambda}, \code{U}, \code{Sigma}, \code{phi}, \code{tau}, and \code{Xi}, each representing the initial MCMC value for the corresponding scFM parameter.
+#' @param priors A list of hyperparameter values. Valid tags are \code{Sigma} and \code{phi}, specifying the hyperparameters for the inverse-gamma prior on error variances (i.e., \eqn{a_{\sigma}}, \eqn{b_{\sigma}}) and the Dirichlet-Laplace prior on factor loadings (i.e., \eqn{\alpha}), respectively.
 #' @param nmcmc An integer specifying the total number of MCMC iterations.
 #' @param nburnin An integer indicating the number of burn-in samples.
 #'
@@ -30,7 +30,7 @@
 #' k_max = 8
 #' priors = list()
 #' priors$Sigma = c(0.1, 0.1)
-#' priors$Phi   = 0.5
+#' priors$phi   = 0.5
 #' 
 #' #################### initialize parameters ####################
 #' # initialize Z using the scaled empirical CDF of each variable
@@ -56,10 +56,9 @@
 #' # initialize the remaining parameters by sampling from their full conditional distributions, 
 #' # given the initialized values of Z, delta, Lambda, and U.
 #' Sigma = diag(1, p)
-#' Phi = rep(1 / k_max, k_max)
+#' phi = rep(1 / k_max, k_max)
 #' tau = 1
-#' Psi = matrix(rexp(p * k_max, rate = 0.5), p, k_max)
-#' 
+#' Xi = matrix(rexp(p * k_max, rate = 0.5), p, k_max)
 #' for (iter in 1 : 1000) 
 #' {
 #'    Sigma[diag(rep(TRUE, p))] = 1 / rgamma(p, shape = priors$Sigma[1] + rep(n / 2, p), 
@@ -69,21 +68,21 @@
 #'    H = rep(NA, k_max)
 #'    for (l in 1 : k_max) 
 #'    {
-#'       H[l] = rgig(1, lambda = priors$Phi - p, chi = 2 * sum(abs(starting$Lambda[ , l])), psi = 1)
+#'       H[l] = rgig(1, lambda = priors$phi - p, chi = 2 * sum(abs(starting$Lambda[ , l])), psi = 1)
 #'    }
 #'    
-#'    Phi = H / sum(H)
+#'    phi = H / sum(H)
 #'    
-#'    tau = rgig(1, lambda = k_max * (priors$Phi - p), chi = 2 * sum(t(abs(starting$Lambda)) / Phi), psi = 1)
+#'    tau = rgig(1, lambda = k_max * (priors$phi - p), chi = 2 * sum(t(abs(starting$Lambda)) / phi), psi = 1)
 #'    
-#'    Psi_tilde = rinvgauss(p * k_max, mean = c(t(Phi * t(tau / abs(starting$Lambda)))))
-#'    Psi = matrix(1 / Psi_tilde, p, k_max)
+#'    Xi_tilde = rinvgauss(p * k_max, mean = c(t(phi * t(tau / abs(starting$Lambda)))))
+#'    Xi = matrix(1 / Xi_tilde, p, k_max)
 #' }
 #' 
 #' starting$Sigma = Sigma
-#' starting$Phi   = Phi
+#' starting$phi   = phi
 #' starting$tau   = tau
-#' starting$Psi   = Psi
+#' starting$Xi    = Xi
 #' 
 #' #################### run our proposed data-augmented Gibbs sampler for scFM ####################
 #' nmcmc   = 10000
@@ -155,9 +154,9 @@ gibbs_scFM = function(x, k, m, starting, priors, nmcmc, nburnin)
    Lambda = starting$Lambda
    U      = starting$U
    Sigma  = starting$Sigma
-   Phi    = starting$Phi
+   phi    = starting$phi
    tau    = starting$tau
-   Psi    = starting$Psi
+   Xi     = starting$Xi
    
    # initialize MCMC samples
    #Z_gibbs      = array(0, dim = c(n, p, nmcmc - nburnin))
@@ -165,102 +164,48 @@ gibbs_scFM = function(x, k, m, starting, priors, nmcmc, nburnin)
    Lambda_gibbs = array(0, dim = c(p, k, nmcmc - nburnin))
    U_gibbs      = array(0, dim = c(n, k, nmcmc - nburnin))
    #Sigma_gibbs  = array(0, dim = c(p, p, nmcmc - nburnin))
-   #Phi_gibbs    = matrix(0, k, nmcmc - nburnin)
+   #phi_gibbs    = matrix(0, k, nmcmc - nburnin)
    #tau_gibbs    = rep(0, nmcmc - nburnin)
-   #Psi_gibbs    = array(0, dim = c(p, k, nmcmc - nburnin))
+   #Xi_gibbs     = array(0, dim = c(p, k, nmcmc - nburnin))
    
    # iterate
    for (iter in 1 : nmcmc)
    {
       # sample Z from its full conditional
       D = sqrt(diag(tcrossprod(Lambda) + Sigma))
-      #for (i in 1 : n)
-      #{
-      #   ind0 = which(x[i, ] == 0)
-      #   ind1 = which(x[i, ] == 1)
-      #   tind = c(ind0, ind1)
-      #   
-      #   cmu    = (tcrossprod(Lambda, U[i, , drop = FALSE]) / D)[tind, ]
-      #   cSigma = (Sigma / D^2)[tind, tind]
-      #   
-      #   lower = rep(-Inf, length(tind))
-      #   if (length(ind1) > 0) lower[length(ind0) + (1 : length(ind1))] = delta[ind1, 1]
-      #   upper = rep(Inf, length(tind))
-      #   if (length(ind0) > 0) upper[1 : length(ind0)] = delta[ind0, 1]
-      #   if (length(ind1) > 0) upper[length(ind0) + (1 : length(ind1))] = delta[ind1, 2]
-      #   
-      #   #zt_new = tmvtnorm::rtmvnorm(1, mean = cmu, sigma = cSigma, lower = lower, upper = upper, 
-      #   #                            algorithm = "gibbs", burn.in.samples = 50, thinning = 1)
-      #   #Z[i, tind] = zt_new
-      #   Z[i, tind] = tmvnsim::tmvnsim(1, length(tind), lower = lower, upper = upper, means = cmu, sigma = cSigma)$samp
-      #   if (any(Z[i, tind] < lower | Z[i, tind] > upper))
-      #      Z[i, tind] = TruncatedNormal::rtmvnorm(1, mu = cmu, sigma = cSigma, lb = lower, ub = upper)
-      #}
       Z = sample_Z(Z, delta, Lambda, U, Sigma, diag(1 / D), x, m)
       
       # sample delta from its full conditional
-      #z0_max = rep(NA, p)
-      #z1_min = rep(NA, p)
-      #z1_max = rep(NA, p)
-      #zo_min = rep(NA, p)
-      #for (j in 1 : p)
-      #{
-      #   z0_max[j] = max(Z[x[ , j] == 0, j])
-      #   z1_min[j] = min(Z[x[ , j] == 1, j])
-      #   z1_max[j] = max(Z[x[ , j] == 1, j])
-      #   zo_min[j] = min(Z[x[ , j] != 0 & x[ , j] != 1, j])
-      #}
-      #delta[ , 1] = runif(p, min = z0_max, max = z1_min)
-      #delta[ , 2] = runif(p, min = z1_max, max = zo_min)
-      delta = sample_delta(delta, Z, x, m)    # Rcpp function to sample delta
+      delta = sample_delta(delta, Z, x, m)    
       
       # sample Lambda from its full conditional
-      #for (j in 1 : p)
-      #{
-      #   invXi = crossprod(U) / Sigma[j, j] + diag(1 / (Psi[j, ] * tau^2 * Phi^2), k)
-      #   invXi = (invXi + t(invXi)) / 2   # guarantee symmetry
-      #   invXi = as(invXi, "sparseMatrix")
-      #   chol_invXi = Matrix::Cholesky(invXi, LDL = FALSE)
-      #   e_Xi = as.vector(Matrix::solve(chol_invXi, rnorm(k), system = "Lt"))
-      #   invXi_eta = crossprod(U, Z[ , j]) / Sigma[j, j]
-      #   eta = as.vector(Matrix::solve(chol_invXi, Matrix::solve(chol_invXi, invXi_eta, system = "L"), system = "Lt"))
-      #   Lambda[j, ] = eta + e_Xi
-      #}
-      Lambda = sample_Lambda(Z, U, Sigma, Phi, tau, Psi)
+      Lambda = sample_Lambda(Z, U, Sigma, phi, tau, Xi)
       
       # sample U from its full conditional
-      #invOmega_j    = crossprod(Lambda, Lambda / diag(Sigma)) + diag(rep(1, k))
-      #invOmega_j    = (invOmega_j + t(invOmega_j)) / 2   # guarantee symmetry
-      #invOmega      = do.call(Matrix::bdiag, rep(list(invOmega_j), n))
-      #chol_invOmega = Matrix::Cholesky(invOmega, LDL = FALSE)
-      #e_Omega       = as.vector(Matrix::solve(chol_invOmega, rnorm(n * k), system = "Lt"))
-      #invOmega_nu   = c(crossprod(Lambda, t(Z) / diag(Sigma)))
-      #nu            = as.vector(Matrix::solve(chol_invOmega, Matrix::solve(chol_invOmega, invOmega_nu, system = "L"), system = "Lt"))
-      #U             = matrix(nu + e_Omega, n, k, byrow = TRUE)
       U = sample_U(Z, Lambda, diag(1 / diag(Sigma)))
       
       # sample Sigma from its full conditional
       Sigma[diag(rep(TRUE, p))] = 1 / rgamma(p, shape = priors$Sigma[1] + rep(n / 2, p), 
                                              rate = priors$Sigma[2] + 0.5 * colSums((Z - tcrossprod(U, Lambda))^2))
       
-      # sample Phi from its full conditional
+      # sample phi from its full conditional
       H = rep(NA, k)
       for (l in 1 : k)
       {
-         H[l] = rgig(1, lambda = priors$Phi - p, chi = 2 * sum(abs(Lambda[ , l])), psi = 1)
+         H[l] = rgig(1, lambda = priors$phi - p, chi = 2 * sum(abs(Lambda[ , l])), psi = 1)
       }
       
-      Phi = H / sum(H)
+      phi = H / sum(H)
       
       # sample tau from its full conditional
-      tau = rgig(1, lambda = k * (priors$Phi - p), chi = 2 * sum(t(abs(Lambda)) / Phi), psi = 1)
+      tau = rgig(1, lambda = k * (priors$phi - p), chi = 2 * sum(t(abs(Lambda)) / phi), psi = 1)
       
-      # sample Psi from its full conditional
-      Psi_tilde = rinvgauss(p * k, mean = c(t(Phi * t(tau / abs(Lambda)))))
-      Psi = matrix(1 / Psi_tilde, p, k)
+      # sample Xi from its full conditional
+      Xi_tilde = rinvgauss(p * k, mean = c(t(phi * t(tau / abs(Lambda)))))
+      Xi = matrix(1 / Xi_tilde, p, k)
       
       # print progress
-      if (iter %% 100 == 0) cat("iter =", iter, "\n")
+      if (iter %% 500 == 0) cat("iter =", iter, "\n")
       
       # store MCMC samples
       if (iter > nburnin)
@@ -270,9 +215,9 @@ gibbs_scFM = function(x, k, m, starting, priors, nmcmc, nburnin)
          Lambda_gibbs[ , , iter - nburnin] = Lambda
          U_gibbs[ , , iter - nburnin]      = U
          #Sigma_gibbs[ , , iter - nburnin]  = Sigma
-         #Phi_gibbs[ , iter - nburnin]      = Phi
+         #phi_gibbs[ , iter - nburnin]      = phi
          #tau_gibbs[iter - nburnin]         = tau
-         #Psi_gibbs[ , , iter - nburnin]    = Psi
+         #Xi_gibbs[ , , iter - nburnin]     = Xi
       }
    }
    
@@ -283,8 +228,8 @@ gibbs_scFM = function(x, k, m, starting, priors, nmcmc, nburnin)
    out$Lambda = Lambda_gibbs
    out$U      = U_gibbs
    #out$Sigma  = Sigma_gibbs
-   #out$Phi    = Phi_gibbs
+   #out$phi    = phi_gibbs
    #out$tau    = tau_gibbs
-   #out$Psi    = Psi_gibbs
+   #out$Xi     = Xi_gibbs
    return(out)
 }
