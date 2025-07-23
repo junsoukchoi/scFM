d16774d6b85c998de60ab5d9647a906421cb4bb9
Junsouk Choi <junsoukchoi@Junsouks-Mac-Studio.local>
2025-07-23 19:53:22 +0900

    Include pre-built vignette.

---
+++ inst/doc/scFM.R
@@ -0,0 +1,16 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  echo = TRUE,
+  message = FALSE,
+  warning = FALSE,
+  collapse = TRUE,
+  comment = NA,
+  eval = TRUE,
+  cache = TRUE,
+  tidy = TRUE,
+  tidy.opts = list(width.cutoff = 60),
+  fig.width = 7,
+  fig.height = 5.25,
+  fig.align = "center"
+)
+
